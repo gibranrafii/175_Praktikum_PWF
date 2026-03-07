b548f461c924fb58b824d27f6f66cc94c67c30b6
gibranrafii <gibranrafi21@gmail.com>
2026-03-07 12:45:04 +0700

    Praktikum 3

---
+++ Praktikum3.md
@@ -0,0 +1,9 @@
+# Praktikum 3
+---
+
+| Keterangan | Screenshot |
+| :---         |     :---:      |
+| Kategori | [](./Screenshot/Prak3/kategori.png) |
+| Product | [](./Screenshot/Prak3/product.png) |
+| User | [](./Screenshot/Prak3/user.png) |
+| ERd | [](./Screenshot/Prak3/ERD.png) |
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            ['name' => 'Test User', 'password' => bcrypt('password')]
+        );
+
+        Product::factory(20)->create();
+        Category::factory(10)->create();
     }
 }
